d2e962d2064b86133d957cee356093b22b5d00d0
Anknoit <personal@ankitjha8891@gmail.com>
2025-04-08 01:02:26 +0530

    initial

---
+++ demo_application/demo_app/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1>Well Hello There</h1>
+<h1>This is my first svelte experience</h1>
+<h3>I Love how the changes I make is directly rendered onto the html page</h3>
